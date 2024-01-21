b45f0e7f830884d16b6a4226e4358cbe0c5e49f9
Brandon <brandon.largeau@gmail.com>
2024-01-21 18:09:16 +0100

    test: add missing vite configs

---
+++ libs/testc/vite.config.ts
@@ -0,0 +1,32 @@
+/// <reference types="vitest" />
+
+import angular from '@analogjs/vite-plugin-angular';
+import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      angular({
+        tsconfig: 'libs/testc/tsconfig.spec.json',
+      }),
+      viteTsConfigPaths(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      reporters: ['default'],
+      coverage: {
+        enabled: true,
+        provider: 'v8',
+        reporter: ['text', 'lcov'],
+      },
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
